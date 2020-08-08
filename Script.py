86966f3dc4c997f90db04ec16030666c62c22f23
Konrad Polkowski <konrad.polkovvski@gmail.com>
2020-08-08 22:31:58 +0200

    this is rare version of difrent ways working on file i want to create some kind of editor. i added few possibilites now i have to marge this all to nice looking program

--- Script.py
+++ Script.py
@@ -1,5 +1,6 @@
 "ok this is my first mechanism to save things but what things :O"
-print("\nWelcome to Book Editor\n")
+
+# # print("\nWelcome to Book Editor\n")
 # # answer = input(
 # #     "what you want to do:\n\n1. Create new page\n2. Edit exist page\n3. Change number of page\n4. Delete\n")
 
@@ -20,36 +21,40 @@ print(lol)
 lista = list(lol)
 dl = int(len(lista))
 print(dl)
+final_list = []
+for i in lista:
+        final_list.append(i.strip())
 
-dict1 = []
-x = 1
-while x <= dl:
-    for line in lista:
-        dict1.append({x: line})
-        x = x+1
-
-
-print(dict1)
-test2.close()
-
-dict1[0] = {1: "input"}
-print(dict1[0])
-print(dict1)
-
-nowe = []
-for x in dict1:
-    for a in x:
-        nowe.append(x[a])
-        print(x[a])
-
-print(nowe)
+print(final_list)
+# dict1 = []
+# x = 1
+# while x <= dl:
+#     for line in lista:
+#         dict1.append({x: line})
+#         x = x+1
 
 
-test3 = open("Saves.txt", "w+")
-for i in nowe:
-    test3.write(i+"\n")
-test3.close()
+# print(dict1)
+# test2.close()
+
+# dict1[0] = {1: "input"}
+# print(dict1[0])
+# print(dict1)
+
+# nowe = []
+# for x in dict1:
+#     for a in x:
+#         nowe.append(x[a])
+#         print(x[a])
+
+# print(nowe)
 
 
-nowe.remove(nowe[7])
-print(nowe)
+# test3 = open("Saves.txt", "w+")
+# for i in nowe:
+#     test3.write(i+"\n")
+# test3.close()
+
+
+# nowe.remove(nowe[7])
+# print(nowe)
