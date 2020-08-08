d508b7477b5f866a3f8f503af2f10d8df817810c
Konrad Polkowski <konrad.polkovvski@gmail.com>
2020-08-08 22:16:01 +0200

    is it working

--- Script.py
+++ Script.py
@@ -1,6 +1,5 @@
 "ok this is my first mechanism to save things but what things :O"
-
-# # print("\nWelcome to Book Editor\n")
+print("\nWelcome to Book Editor\n")
 # # answer = input(
 # #     "what you want to do:\n\n1. Create new page\n2. Edit exist page\n3. Change number of page\n4. Delete\n")
 
